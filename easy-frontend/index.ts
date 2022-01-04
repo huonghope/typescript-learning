f5443957b0ad1a6483b9313981d5aeb18254ff74
huonghope <hopekr@dongguk.edu>
2022-01-04 16:10:09 +0900

    add generics

--- easy-frontend/index.ts
+++ easy-frontend/index.ts
@@ -54,18 +54,71 @@ interface Student {
 // K : key
 // V : Value
 // T : type
+// Thường dùng để định nghĩa nhanh 1 kiểu dữ liệu, có thể khó hiểu với nembie
 interface List<T>{
   length: number;
   // Truy xuất những phần tử theo index number,
   [index: number]: T, //xuất ra 1 kiểu dữ liệu là T, không biết là kiểu gì
   // [index: number]: T | string, //xuất ra 1 kiểu dữ liệu là T, không biết là kiểu gì
+  // [index: string]: T | number: sẽ hiểu đây là 1 dạng object, truyền vào key và nhận lại key
+  // Phải convert được kiểu dữ liệu có các key khác nữa
 }
+
 
 // định nghĩ các phần tử có kiểu là number
 const myNumberList: List<number> = [1, 2, 3];
 const myNumberListString: List<string> = ['hello', 'hi'];
+console.log(myNumberListString[1]) // input length of array
 console.log(myNumberListString.length) // input length of array
 
 const numberList: Array<number> = [1, 2, 3];
 const wordList: Array<string> = ['hello', 'hi', 'bye'];
 const studentList: Array<Student> = [{id: 1, name: "My"}, {id: 2, name: "Jack"}];
+const listStudent: List<Student> = [{id: 1, name: "My"}, {id: 2, name: "Jack"}];
+
+// keyof operator - input key of interface
+// list of key of everything datatypes
+type StudentKeys = keyof Student;
+const keys : StudentKeys = 'age';
+
+console.log(typeof 123);
+console.log(typeof window !== 'undefined'); // check server and client
+
+
+// Map types
+interface Student { 
+  id: number;
+  name : string;
+}
+
+// deceleration key
+type MappedTypes = { 
+  [Key in keyof Student]: boolean;
+}
+
+// Ví dụ gọi API, dữ liệu trả về
+export interface PaginationParams {
+  _limit: number;
+  _page: number;
+  _totalRows: number;
+}
+
+export interface ListResponse<T>{
+  data: [];
+  pagination: PaginationParams;
+}
+
+export interface ListParams {
+  _page?: number;
+  _limit?: number;
+  _sort?: string;
+  _order: 'asc' | 'desc';
+
+  [key: string] : any;
+}
+
+//call API Example
+getAll(): Promise<ListResponse<Student>> {
+  const url = '/cities';
+  return axiosClient.get(url, { params: { _page: 1, _limit: 10}})
+}
