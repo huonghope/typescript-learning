5b7f40fa5e7d1359637f6102fd226c907bc0bf11
huonghope <hopekr@dongguk.edu>
2022-01-04 15:44:19 +0900

    add ppt files

--- easy-frontend/README.md
+++ easy-frontend/README.md
@@ -1,4 +1,7 @@
 https://kulshekhar.github.io/ts-jest/docs/getting-started/installation
+https://www.typescriptlang.org/docs/handbook/typescript-from-scratch.html
+https://www.typescriptlang.org/tsconfig
+
 
 ```javascript
 tsc // compile typescript to javascript
@@ -7,3 +10,17 @@ yarn ts-jest config:init // create jet.config.js
 tsc --init // create tsconfig.json
 noEmitOnError // option compile, if file have error 
 ```
+
+1, strictBindCallApply: đảm bảo bind, call và apply được gọi đúng args
+2, strictFunctionTypes: đảm bảo đúng dữ liệu cho parameters của function (ko áp dụngcho method)
+3 strictPropertyInitializer: đảm bảo thuộc tính của class phải được khởi tạo
+4 noImplicitThis: đảm bảo là this không mang kiểu dữ liệu any
+5 useUnknownInCatchVariables: đẩm bảo là biến trong catch là kiểu unknown
+
+Primitive: number, boolean, string, number, undefined, symbol
+Reference: array, object, function
+
+Khuyên dùng sử dụng interface sau đó mới ưu tiên type: https://drive.google.com/file/d/1LNrtSnD7nJRjvYSj_Zgu0vBYi16m7_0p/view
+Dùng as để ép kiểu dữ liệu
+
+Generic : kiểu dữ liệu mà có nhận tham số và trả về kiểu dữ liệu tương ứng
--- easy-frontend/index.ts
+++ easy-frontend/index.ts
@@ -5,3 +5,67 @@ const num : number = 123;
 let studentName : string = "Huong";
 let isActive: boolean = false;
 let numberLst : number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+
+
+const studentA = { id : 1, name : "Student"};
+const studentB = studentB;
+studentA === studentB;
+
+interface Student {
+  id : number;
+  name : string;
+  age : number;
+  gender : string;
+}
+
+const studentJack : Student = { id : 1, name : "Jack", age : 12, gender : "Man"};
+
+function sayHello(){
+  console.log("Hello");
+}
+
+function sum(a: number, b: number){
+  return a + b;
+}
+function sumNumber(a: number, b: number): number{
+  return a + b;
+}
+
+// add question mask ? to tell ty this an optional parameter
+function getLength(numberList?: number[]){
+  return Array.isArray(numberList) ? numberList.length : 0;
+}
+
+// default paramater
+function getLengthHaveDefault(numberList: number[] = []){
+  return numberList.length : 0;
+}
+
+// Enum type
+https://drive.google.com/file/d/1GT4SNNZxe329lO7uwWrBj3MDn-AoaGIo/view
+// Dùng với các static data, không lên dùng cho data linh động
+
+// Generics
+interface Student {
+  id: number;
+  name: string;
+}
+
+// K : key
+// V : Value
+// T : type
+interface List<T>{
+  length: number;
+  // Truy xuất những phần tử theo index number,
+  [index: number]: T, //xuất ra 1 kiểu dữ liệu là T, không biết là kiểu gì
+  // [index: number]: T | string, //xuất ra 1 kiểu dữ liệu là T, không biết là kiểu gì
+}
+
+// định nghĩ các phần tử có kiểu là number
+const myNumberList: List<number> = [1, 2, 3];
+const myNumberListString: List<string> = ['hello', 'hi'];
+console.log(myNumberListString.length) // input length of array
+
+const numberList: Array<number> = [1, 2, 3];
+const wordList: Array<string> = ['hello', 'hi', 'bye'];
+const studentList: Array<Student> = [{id: 1, name: "My"}, {id: 2, name: "Jack"}];
